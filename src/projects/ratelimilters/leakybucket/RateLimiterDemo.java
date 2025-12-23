b623f4a5747850723b834484ea89bb770f430ebb
Tejprakash18 <tej22upa.dhyay@gmail.com>
2025-12-23 08:21:44 +0530

    rate limiter, Leackybucket

--- src/projects/ratelimilters/leakybucket/RateLimiterDemo.java
+++ src/projects/ratelimilters/leakybucket/RateLimiterDemo.java
@@ -2,8 +2,8 @@ package projects.ratelimilters.leakybucket;
 
 public class RateLimiterDemo {
     public static void main(String[] args) throws InterruptedException {
-        RateLimiter rateLimiter = new LeakyBucketRateLimiter(5,2);
-        for (int i = 0; i < 10; i++) {
+        RateLimiter rateLimiter = new LeakyBucketRateLimiter(2,2);
+        for (int i = 1; i <= 12; i++) {
             System.out.println("Request " + i + " allowed " + rateLimiter.tryAcquire());
             Thread.sleep(300);
         }
